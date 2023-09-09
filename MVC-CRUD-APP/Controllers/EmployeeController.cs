99f2de8bedb5976609bc8492e32f0e842f07cd35
bukani671 <you@example.com>
2023-09-09 23:02:58 +0200

    adding contollers

--- MVC-CRUD-APP/Controllers/EmployeeController.cs
+++ MVC-CRUD-APP/Controllers/EmployeeController.cs
@@ -1,15 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD_APP.Data;
+using MVC_CRUD_APP.Models;
+using MVC_CRUD_APP.Models.Domain;
 
 namespace MVC_CRUD_APP.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly MVCCrudDbContext mVCCrudDbContext;
+        public EmployeeController(MVCCrudDbContext mVCCrudDbContext)
+        {
+            this.mVCCrudDbContext = mVCCrudDbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+
+            };
+         await mVCCrudDbContext.Employees.AddAsync(employee);
+         await mVCCrudDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+
+        }
 
     }
 }
---
+++ MVC-CRUD-APP/Models/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_CRUD_APP.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
