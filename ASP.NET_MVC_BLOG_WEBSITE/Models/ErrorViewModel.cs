06465c14521fd8237d4f87673e584be2b40b531c
bukani671 <89846166+bukani671@users.noreply.github.com>
2023-09-12 11:47:31 +0200

    Delete ASP.NET_MVC_BLOG_WEBSITE directory

--- ASP.NET_MVC_BLOG_WEBSITE/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_MVC_BLOG_WEBSITE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
